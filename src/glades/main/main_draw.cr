b1639c822e82ba10081c224c0055d4285d277ed6
D. Shwagginz <93965353+D-Shwagginz@users.noreply.github.com>
2024-01-20 02:27:16 -0700

    no cube

--- src/glades/main/main_draw.cr
+++ src/glades/main/main_draw.cr
@@ -5,8 +5,6 @@ module Glades
     Raylib.clear_background(Raylib::RAYWHITE)
     unless @@player.nil?
       Raylib.begin_mode_3d(@@player.as(Player).camera)
-      Raylib.draw_cube(Raylib::Vector3.new(x: 0, y: 0, z: 0), 2.0, 2.0, 2.0, Raylib::RED)
-      Raylib.draw_cube_wires(Raylib::Vector3.new(x: 0, y: 0, z: 0), 2.0, 2.0, 2.0, Raylib::MAROON)
       Raylib.draw_grid(10, 1.0)
       Raylib.end_mode_3d
     end
